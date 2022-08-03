c734c76676fc14edd332ef8807b6049565c1c23b
Oliver Klingefjord <oliver@kry.se>
2022-08-03 22:00:02 +0200

    scrape twitter finalization

--- functions/scrapeTwitter.f.js
+++ functions/scrapeTwitter.f.js
@@ -1,6 +1,6 @@
 /**
- * Every 20 minutes, fetch users that haven't been updated since a day ago.
- * Update the
+ * Every 15 minutes, fetch users that haven't been updated since a day ago.
+ * For every user, fetch their latest mention tweets recursively, until they are older since last time the function was run.
  */
 
 const functions = require("firebase-functions");
@@ -8,34 +8,40 @@ const admin = require("./admin");
 
 const TwitterApi = require("twitter-api-v2").TwitterApi;
 
-// Set up the twitter client.
-const frequencyMinutes = 15;
+module.exports = functions.pubsub
+  .schedule(`every 60 minutes`)
+  .onRun(async (_) => {
+    // The time when the function starts.
+    const start = Date.now();
 
-// module.exports = functions.pubsub
-// .schedule(`every ${frequencyMinutes} minutes`)
-// .onRun(async (context) => {
-module.exports = functions.https.onCall(async (data) => {
-  const lastFunctionCall = new Date(Date.now() - 1000 * 60 * frequencyMinutes);
+    // The twitter client.
+    const client = new TwitterApi({
+      appKey: process.env.CONSUMER_KEY,
+      appSecret: process.env.CONSUMER_SECRET,
+      accessToken: process.env.ACCESS_TOKEN_KEY,
+      accessSecret: process.env.ACCESS_TOKEN_SECRET,
+    });
 
-  const client = new TwitterApi({
-    appKey: process.env.CONSUMER_KEY,
-    appSecret: process.env.CONSUMER_SECRET,
-    accessToken: process.env.ACCESS_TOKEN_KEY,
-    accessSecret: process.env.ACCESS_TOKEN_SECRET,
+    // Get users that haven't been crawled in a day.
+    const users = await admin
+      .firestore()
+      .collection("users")
+      .where("last_crawled_at", "<", new Date(Date.now() - 1000 * 60 * 60 * 24))
+      .get();
+
+    console.log(`About to crawl ${users.docs.length} users.`);
+
+    // Crawl tweets for each user.
+    for (const user of users.docs) {
+      const lastCrawledAt = user.data()["last_crawled_at"]?.toDate();
+
+      await crawlUser(user.id, client, lastCrawledAt);
+    }
+
+    console.log(
+      `Finished crawling. Took ${(Date.now() - start) / 1000} seconds.`
+    );
   });
-
-  // Get users that haven't been crawled in a while.
-  const users = await admin
-    .firestore()
-    .collection("users")
-    .where("last_crawled", "<", lastFunctionCall)
-    .get();
-
-  // Crawl tweets for each user.
-  for (const user of users.docs) {
-    await crawlUser(user.id, client, user.last_crawled);
-  }
-});
 
 /**
  * Crawl recent tweets for a user and update firestore with the result.
@@ -54,8 +60,10 @@ async function crawlUser(userId, client, lastCrawl) {
     let batch = admin.firestore().batch();
 
     for (let i = 0; i < tweets.length; i++) {
+      //
       // There is a 500 write limit to firestore batches.
-      // If the limit is reached, commit the batch before continuing.
+      // If the limit is reached, commit and refresh the batch before continuing.
+      //
       if (i >= 499) {
         await batch.commit();
 
@@ -81,13 +89,15 @@ async function crawlUser(userId, client, lastCrawl) {
 
     await batch.commit();
 
-    // Update the `last_crawled` property of the user.
+    //
+    // Update the `last_crawled_at` property of the user.
+    //
     await admin
       .firestore()
       .collection("users")
       .doc(userId)
       .update({
-        last_crawled: admin.firestore.Timestamp.fromDate(new Date()),
+        last_crawled_at: admin.firestore.Timestamp.fromDate(new Date()),
       });
 
     console.log(`Finished crawling ${tweets.length} tweets for ${userId}.`);
@@ -112,14 +122,18 @@ async function crawlUser(userId, client, lastCrawl) {
 async function getReplyTweetsRecursively(
   id,
   client,
-  lastCrawl,
-  paginationCount = 0,
+  lastCrawledAt,
+  paginationCount = 1,
   paginationToken = null,
   accumulatedTweets = [],
   paginationLimit = 100
 ) {
+  console.log(lastCrawledAt?.toISOString());
+
   let params = {
     max_results: 100,
+    start_time: lastCrawledAt?.toISOString(),
+    exclude: ["retweets"],
     expansions: ["author_id", "referenced_tweets.id", "in_reply_to_user_id"],
     "tweet.fields": ["author_id", "referenced_tweets", "text"],
   };
@@ -137,11 +151,7 @@ async function getReplyTweetsRecursively(
   }
 
   // Only include tweets that have a referenced tweet.
-  let tweets = response.data.filter(
-    (t) =>
-      t.referenced_tweets?.length > 0 &&
-      t.referenced_tweets[0].type !== "retweeted"
-  );
+  let tweets = response.data.filter((t) => t.referenced_tweets?.length > 0);
 
   // Attach the full referenced tweet object to each tweet.
   tweets = tweets.map((tweet) => {
@@ -162,31 +172,23 @@ async function getReplyTweetsRecursively(
     tweets.filter((t) => t.referenced_tweet)
   );
 
-  //
-  // Only recursively fetch tweets newer than the last crawl for the user.
-  //
-  const isFromBeforeLastCrawl =
-    accumulatedTweets.sort((t) => t.created_at).pop().created_at <= lastCrawl;
-
-  if (accumulatedTweets.sort((t) => t.created_at))
-    if (
-      response.meta &&
-      response.meta.next_token &&
-      paginationCount < paginationLimit &&
-      !isFromBeforeLastCrawl
-    ) {
-      //
-      // Fetch tweets for next pagination if appliable.
-      //
-      return getReplyTweetsRecursively(
-        id,
-        client,
-        lastCrawl,
-        ++paginationCount,
-        response.meta.next_token,
-        accumulatedTweets
-      );
-    } else {
-      return accumulatedTweets;
-    }
+  if (
+    response.meta &&
+    response.meta.next_token &&
+    paginationCount < paginationLimit
+  ) {
+    //
+    // Fetch tweets for next pagination if appliable.
+    //
+    return getReplyTweetsRecursively(
+      id,
+      client,
+      lastCrawledAt,
+      ++paginationCount,
+      response.meta.next_token,
+      accumulatedTweets
+    );
+  } else {
+    return accumulatedTweets;
+  }
 }
