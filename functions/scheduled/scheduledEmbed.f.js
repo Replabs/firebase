47132e1b1144fdc7ce36e2484b5751924dbadc3f
Oliver Klingefjord <oliver@kry.se>
2022-08-14 07:39:47 +0200

    change frequency

--- functions/scheduled/scheduledEmbed.f.js
+++ functions/scheduled/scheduledEmbed.f.js
@@ -1,10 +1,10 @@
 /**
- * Every 10th minute, embed unembedded tweets.
+ * Every 15th minute, embed unembedded tweets.
  */
 
 const functions = require("firebase-functions");
 const embed = require("../embed");
 
 module.exports = functions.pubsub
-  .schedule(`*/10 * * * *`) // Every 10th minute.
+  .schedule(`/15 * * * *`) // Every 10th minute.
   .onRun(async (_) => embed());
