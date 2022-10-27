7aab4cd0a3385374265e3f86847279168536b4fc
Oliver Klingefjord <oliver@kry.se>
2022-10-27 12:24:49 +0200

    make scripts run less often

--- functions/scheduled/scheduledEmbed.f.js
+++ functions/scheduled/scheduledEmbed.f.js
@@ -1,10 +1,10 @@
 /**
- * Every 10th minute, embed unembedded tweets.
+ * Every day, embed tweets.
  */
 
 const functions = require("firebase-functions");
 const embed = require("../embed");
 
 module.exports = functions.pubsub
-  .schedule(`*/10 * * * *`) // Every 10th minute.
+  .schedule(`0 0 * * *`) // Every day.
   .onRun(async (_) => embed());
--- functions/scheduled/scheduledScrape.f.js
+++ functions/scheduled/scheduledScrape.f.js
@@ -1,11 +1,10 @@
 /**
- * Every hour, scrape tweets for all lists since last time
- * a scrape was performed.
+ * Every 24 hours, fetch tweets.
  */
 
 const functions = require("firebase-functions");
 const scrape = require("../scrape");
 
 module.exports = functions.pubsub
-  .schedule(`0 * * * *`) // Every hour at minute 0.
+  .schedule(`0 0 * * *`) // Every day at minute 0.
   .onRun(async (_) => scrape());
